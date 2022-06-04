03ff4ea92bbdfe30256b184c71b1348a4e6027e1
StefanosTouf <stefetoufe@gmail.com>
2022-06-04 14:58:32 +0300

    Seperate Websocker configuration into its own package

--- src/main/kotlin/com/steft/chatserver/websocket/configuration/WebsocketMappings.kt
+++ src/main/kotlin/com/steft/chatserver/websocket/configuration/WebsocketMappings.kt
@@ -1,4 +1,14 @@
 package com.steft.chatserver.websocket.configuration
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
+import org.springframework.web.reactive.socket.WebSocketHandler
+
+@Configuration
 class WebsocketMappings {
+    @Bean
+    fun webSocketHandlerMapping(handler: WebSocketHandler): HandlerMapping =
+        SimpleUrlHandlerMapping(mapOf("/messages/*" to handler), 1)
 }
