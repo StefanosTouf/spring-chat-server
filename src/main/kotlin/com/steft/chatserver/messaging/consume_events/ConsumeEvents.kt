d10497e2d82fd1b754f2272e200f0d931792abfa
StefanosTouf <stefetoufe@gmail.com>
2022-06-04 14:57:57 +0300

    clean up

--- src/main/kotlin/com/steft/chatserver/messaging/consume_events/ConsumeEvents.kt
+++ src/main/kotlin/com/steft/chatserver/messaging/consume_events/ConsumeEvents.kt
@@ -4,6 +4,5 @@ import com.steft.chatserver.model.Event
 import com.steft.chatserver.model.Serialized
 import com.steft.chatserver.model.UserId
 import reactor.core.publisher.Flux
-import reactor.rabbitmq.Sender
 
 interface ConsumeEvents: (UserId) -> Flux<Serialized<Event>>
