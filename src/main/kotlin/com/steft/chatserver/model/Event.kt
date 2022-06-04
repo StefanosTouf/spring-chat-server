b73814163d3887fbe9e05b81bc28d35b698a2f62
StefanosTouf <stefetoufe@gmail.com>
2022-06-04 14:58:12 +0300

    Enable polymorphic serialization

--- src/main/kotlin/com/steft/chatserver/model/Event.kt
+++ src/main/kotlin/com/steft/chatserver/model/Event.kt
@@ -1,29 +1,37 @@
 package com.steft.chatserver.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@Serializable
+sealed class Event
 
 @Serializable
-sealed interface Event
-
+@SerialName("MESSAGE")
 data class Message(
     val messageId: MessageId,
     val from: UserId,
     val to: UserId,
-    val body: String) : Event
+    val body: String) : Event()
 
+@Serializable
+@SerialName("ACK")
 data class Ack(
     val messageId: MessageId,
     val from: UserId,
-    val to: UserId) : Event
+    val to: UserId) : Event()
 
 @Serializable
-sealed interface UntaggedEvent
+sealed class UntaggedEvent
 
+@Serializable
+@SerialName("MESSAGE")
 data class UntaggedMessage(
     val to: UserId,
-    val body: String) : UntaggedEvent
+    val body: String) : UntaggedEvent()
 
+@Serializable
+@SerialName("ACK")
 data class UntaggedAck(
     val to: UserId,
-    val messageId: MessageId) : UntaggedEvent
+    val messageId: MessageId) : UntaggedEvent()
