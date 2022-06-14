66ae02c4f275f16b19afae2acfaa52fa5d1c65d9
StefanosTouf <stefetoufe@gmail.com>
2022-06-15 00:37:20 +0300

    Add base redis skeleton

--- src/main/kotlin/com/steft/chatserver/redis/register_user/RegisterUser.kt
+++ src/main/kotlin/com/steft/chatserver/redis/register_user/RegisterUser.kt
@@ -3,4 +3,4 @@ package com.steft.chatserver.redis.register_user
 import com.steft.chatserver.model.UserId
 import reactor.core.publisher.Mono
 
-interface RegisterUser: (UserId) -> Mono<Boolean>
+interface RegisterUser: (UserId) -> Mono<Unit>
--- src/main/kotlin/com/steft/chatserver/redis/register_user/impl/RegisterUserImpl.kt
+++ src/main/kotlin/com/steft/chatserver/redis/register_user/impl/RegisterUserImpl.kt
@@ -1,5 +1,6 @@
 package com.steft.chatserver.redis.register_user.impl
 
+import com.steft.chatserver.exception.UserQueueRegistrationException
 import com.steft.chatserver.model.OwnedRabbitQueue
 import com.steft.chatserver.model.RabbitQueue
 import com.steft.chatserver.model.UserId
@@ -13,7 +14,15 @@ class RegisterUserImpl(
     private val redisOps: ReactiveValueOperations<UserId, RabbitQueue>,
     private val ownedRabbitQueue: OwnedRabbitQueue) : RegisterUser {
 
-    override fun invoke(user: UserId): Mono<Boolean> =
-        redisOps.set(user, ownedRabbitQueue.rabbitQueue)
+    override fun invoke(user: UserId): Mono<Unit> =
+        redisOps
+            .set(user, ownedRabbitQueue.rabbitQueue)
+            .flatMap { isSuccessful ->
+                if (isSuccessful)
+                    Mono.just(Unit)
+                else
+                    UserQueueRegistrationException("Couldn't register user with id $user")
+                        .let { Mono.error(it) }
+            }
 
 }
