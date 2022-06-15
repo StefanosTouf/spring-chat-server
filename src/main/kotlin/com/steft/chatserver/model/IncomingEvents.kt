3c76d722b621c1277004502bd08ff8ecdf38f8ee
StefanosTouf <stefetoufe@gmail.com>
2022-06-15 17:57:20 +0300

    use serialized events in incoming events

--- src/main/kotlin/com/steft/chatserver/model/IncomingEvents.kt
+++ src/main/kotlin/com/steft/chatserver/model/IncomingEvents.kt
@@ -2,5 +2,4 @@ package com.steft.chatserver.model
 
 import reactor.core.publisher.Flux
 
-@JvmInline
-value class IncomingEvents(val events: Flux<Event>)
+data class IncomingEvents(val events: Flux<Serialized<Event>>)
