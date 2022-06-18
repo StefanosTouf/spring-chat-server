aad09521f9e1d54e92eba35e4c27ac9746122abd
StefanosTouf <stefetoufe@gmail.com>
2022-06-18 23:40:47 +0300

    Tidy up ControlInterceptor

--- src/main/kotlin/com/steft/chatserver/service/control_interceptor/impl/ControlInterceptorImpl.kt
+++ src/main/kotlin/com/steft/chatserver/service/control_interceptor/impl/ControlInterceptorImpl.kt
@@ -24,60 +24,51 @@ class ControlInterceptorImpl : ControlInterceptor {
                 Mono.empty()
         }
 
+    private fun outputNonAcknowledgedOnInterval(interval: Duration): (Flux<Event>) -> Flux<Event.Message> =
+        { messages ->
+            List<Map<EventId, Event.Message>>(retries) { HashMap() }
+                .let { maps ->
+                    messages
+                        .buffer(interval)
+                        .scan(maps) { maps, events ->
+                            val shiftedMaps =
+                                maps.drop(1)
+                                    .plus(HashMap())
+
+                            events.fold(shiftedMaps) { maps, event ->
+                                when (event) {
+                                    is Event.Ack ->
+                                        maps.map { it.minus(event.eventId) }
+
+                                    is Event.Message ->
+                                        maps.mapIndexed { index, map ->
+                                            if (index == maps.lastIndex)
+                                                map.plus(event.eventId to event)
+                                            else
+                                                map
+                                        }
+                                }
+                            }
+                        }
+                        .flatMapIterable { it.asIterable().flatMap { it.asIterable() } }
+                        .map { it.value }
+                }
+        }
+
     override fun invoke(
         fromClient: Flux<Event.Message>,
         toClient: Flux<Event>): Pair<Flux<Event.Message>, Flux<Event.Message>> =
 
         toClient
-            .publish()
-            .refCount(2)
-            .let { toClient ->
-
-                fromClient
-                    .publish()
-                    .refCount(2)
-                    .let { fromClient ->
-
-                        val maps: List<Map<EventId, Event.Message>> =
-                            MutableList<Map<EventId, Event.Message>>(retries) {
-                                HashMap()
-                            }
-
-                        val toResend =
-                            toClient
-                                .filter { it !is Event.Message }
-                                .mergeWith(fromClient)
-                                .buffer(Duration.ofMillis(1000))
-                                .scan(maps) { maps, events ->
-                                    val shiftedMaps =
-                                        maps.drop(1)
-                                            .plus(HashMap())
-
-                                    events.fold(shiftedMaps) { maps, event ->
-                                        when (event) {
-                                            is Event.Ack ->
-                                                maps.map { it.minus(event.eventId) }
-
-                                            is Event.Message ->
-                                                maps.mapIndexed { index, map ->
-                                                    if (index == maps.lastIndex)
-                                                        map.plus(event.eventId to event)
-                                                    else
-                                                        map
-                                                }
-
-                                        }
-                                    }
-                                }
-                                .sample(Duration.ofMillis(1000))
-                                .flatMapIterable { it.asIterable().flatMap { it.asIterable() } }
-                                .map { it.value }
-
-                        toClient.filterMessages() to fromClient.mergeWith(toResend)
-
-                    }
-
+            .filter { it !is Event.Message }
+            .mergeWith(fromClient)
+            .transform(
+                outputNonAcknowledgedOnInterval(
+                    Duration.ofMillis(2000)))
+            .let { toResend ->
+                toClient.filterMessages() to
+                        fromClient.mergeWith(toResend)
             }
 
-
 }
+
