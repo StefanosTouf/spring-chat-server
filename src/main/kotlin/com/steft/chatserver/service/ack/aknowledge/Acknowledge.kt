14e2960ee6d93c2a4422801dbdeae45f50eaa58f
StefanosTouf <stefetoufe@gmail.com>
2022-06-23 16:30:14 +0300

    Remove redis based acknowledgement services

--- src/main/kotlin/com/steft/chatserver/service/ack/aknowledge/Acknowledge.kt
+++
@@ -1,6 +0,0 @@
-package com.steft.chatserver.service.ack.aknowledge
-
-import com.steft.chatserver.model.EventId
-import reactor.core.publisher.Mono
-
-interface Acknowledge: (EventId) -> Mono<Void>
--- src/main/kotlin/com/steft/chatserver/service/ack/get_expired_unaknowledged/GetExpiredUnaknowledged.kt
+++
@@ -1,6 +0,0 @@
-package com.steft.chatserver.service.ack.get_expired_unaknowledged
-
-import com.steft.chatserver.model.Event
-import reactor.core.publisher.Flux
-
-interface GetExpiredUnaknowledged: () -> Flux<Event>
