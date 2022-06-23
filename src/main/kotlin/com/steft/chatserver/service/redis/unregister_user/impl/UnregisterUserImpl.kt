8a16df99de7001237fad91a22f5fba8fd2060e79
StefanosTouf <stefetoufe@gmail.com>
2022-06-23 21:32:23 +0300

    Implement correct function in UnregisterUser

--- src/main/kotlin/com/steft/chatserver/service/redis/unregister_user/impl/UnregisterUserImpl.kt
+++ src/main/kotlin/com/steft/chatserver/service/redis/unregister_user/impl/UnregisterUserImpl.kt
@@ -1,16 +1,15 @@
 package com.steft.chatserver.service.redis.unregister_user.impl
 
 import com.steft.chatserver.exception.UserQueueRegistrationException
-import com.steft.chatserver.model.OwnedRabbitQueue
 import com.steft.chatserver.model.UserId
-import com.steft.chatserver.service.redis.register_user.RegisterUser
+import com.steft.chatserver.service.redis.unregister_user.UnregisterUser
 import org.springframework.data.redis.core.ReactiveValueOperations
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
 @Service
 class UnregisterUserImpl(
-    private val redisOps: ReactiveValueOperations<String, String>) : RegisterUser {
+    private val redisOps: ReactiveValueOperations<String, String>) : UnregisterUser {
 
     override fun invoke(user: UserId): Mono<Unit> =
         redisOps
