d280d0a0d269718f0105dac008133fb53c80b6d6
StefanosTouf <stefetoufe@gmail.com>
2022-06-23 21:35:20 +0300

    Prettify RouteEventsImpl

--- src/main/kotlin/com/steft/chatserver/service/route_events/impl/RouteEventsImpl.kt
+++ src/main/kotlin/com/steft/chatserver/service/route_events/impl/RouteEventsImpl.kt
@@ -4,6 +4,7 @@ import com.steft.chatserver.configuration.messaging.MessagingProperties
 import com.steft.chatserver.model.Event
 import com.steft.chatserver.model.RabbitQueue
 import com.steft.chatserver.model.Serialized
+import com.steft.chatserver.service.messaging.send_outbound.SendOutbound
 import com.steft.chatserver.service.redis.get_queue.GetQueue
 import com.steft.chatserver.service.route_events.RouteEvents
 import com.steft.chatserver.util.serde.json.serialize
@@ -12,26 +13,21 @@ import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.rabbitmq.OutboundMessage
-import reactor.rabbitmq.Sender
 import java.time.Duration
 
 @Service
 class RouteEventsImpl(
     private val messagingProperties: MessagingProperties,
-    private val sender: Sender,
+    private val sendOutbound: SendOutbound,
     private val getQueue: GetQueue) : RouteEvents {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
     private fun toOutbound(queue: RabbitQueue): (Serialized<Event>) -> OutboundMessage = { (data) ->
-        data
-            .encodeToByteArray()
-            .let { bytes ->
-                OutboundMessage(
-                    messagingProperties.messagingExchangeName,
-                    queue.string,
-                    bytes)
-            }
+        OutboundMessage(
+            messagingProperties.messagingExchangeName,
+            queue.string,
+            data.encodeToByteArray())
     }
 
     override fun invoke(incoming: Flux<Event>): Flux<Void> =
@@ -45,10 +41,10 @@ class RouteEventsImpl(
                         events
                             .transform(::serialize)
                             .map(toOutbound(queue))
-                            .let(sender::send)
                     }
                     .timeout(Duration.ofMillis(10000), Mono.empty())
                     .doOnComplete { log.info("Timeout group ${events.key()}") }
+                    .let(sendOutbound)
             }
 
 }
