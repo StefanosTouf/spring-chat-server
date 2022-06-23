e0c56de708e752b8fed11190dc1189770a90161d
StefanosTouf <stefetoufe@gmail.com>
2022-06-23 16:37:18 +0300

    Include acknowledgement properties in start up

--- src/main/kotlin/com/steft/chatserver/ChatterApplication.kt
+++ src/main/kotlin/com/steft/chatserver/ChatterApplication.kt
@@ -2,13 +2,16 @@
 
 package com.steft.chatserver
 
-import com.steft.chatserver.messaging.configuration.MessagingProperties
+import com.steft.chatserver.configuration.messaging.MessagingProperties
+import com.steft.chatserver.service.manage_acknowledgements.AcknowledgementProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 
-@EnableConfigurationProperties(MessagingProperties::class)
+@EnableConfigurationProperties(
+    MessagingProperties::class,
+    AcknowledgementProperties::class)
 @SpringBootApplication
 class ChatterApplication
 
