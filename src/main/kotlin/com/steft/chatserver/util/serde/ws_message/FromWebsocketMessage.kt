aa29223b38adac062d9c5de97e23013dc5611a93
StefanosTouf <stefetoufe@gmail.com>
2022-06-23 21:35:58 +0300

    Remove untagged message from fromWebsocketMessage function

--- src/main/kotlin/com/steft/chatserver/util/serde/ws_message/FromWebsocketMessage.kt
+++ src/main/kotlin/com/steft/chatserver/util/serde/ws_message/FromWebsocketMessage.kt
@@ -1,25 +1,19 @@
 package com.steft.chatserver.util.serde.ws_message
 
 import com.steft.chatserver.model.Event
-import com.steft.chatserver.model.UntaggedMessage
 import com.steft.chatserver.model.UserId
 import com.steft.chatserver.util.serde.json.deserialize
-import com.steft.chatserver.util.tag.tag
 import org.springframework.web.reactive.socket.WebSocketMessage
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 import java.nio.charset.StandardCharsets
 
-fun fromWebsocketMessage(userId: UserId): (Flux<WebSocketMessage>) -> Flux<Event.Message> =
-    { messages ->
-        messages
-            .map {
-                it.payload
-                    .asByteBuffer()
-                    .let(StandardCharsets.UTF_8::decode)
-                    .toString()
-            }
-            .transform { deserialize<UntaggedMessage>(it) }
-            .map(UntaggedMessage.tag(userId))
-    }
+fun fromWebsocketMessage(messages: Flux<WebSocketMessage>): Flux<Event.Message> =
+    messages
+        .map {
+            it.payload
+                .asByteBuffer()
+                .let(StandardCharsets.UTF_8::decode)
+                .toString()
+        }
+        .transform { deserialize<Event.Message>(it) }
 
